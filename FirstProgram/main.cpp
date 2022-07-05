67d56f9bcdcfc9575bb7a9328fb8c8c879a9cea4
Julian Oseng <julianoseng@protonmail.com>
2022-07-05 16:53:49 -0500

    creating basic program

--- FirstProgram/main.cpp
+++ FirstProgram/main.cpp
@@ -8,6 +8,7 @@ int main() {
     std::cin >> favorite_number;
 
     std::cout << "Amazing!! That's my favorite number too!" << std::endl;
+    std::cout << "No really! " << favorite_number << " is my favorite number too!" << std::endl;
     
     return 0;
 }
